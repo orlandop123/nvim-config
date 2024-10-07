f1d9e7dc6c71f7cb913d234a777a6e34ec5f2b00
orlandop123 <orlandoperezjr01@gmail.com>
2024-10-07 07:27:12 -0400

    updated coc.nvim

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -1,12 +1,3 @@
-inoremap <silent><expr> <TAB>
-      \ coc#pum#visible() ? coc#pum#next(1) :
-      \ CheckBackspace() ? "\<Tab>" :
-      \ coc#refresh()
-inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
-
-function! CheckBackspace() abort
-  let col = col('.') - 1
-  return !col || getline('.')[col - 1]  =~# '\s'
-endfunction
-
-inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm() : "\<CR>"
+{
+  "suggest.noselect": false
+}
